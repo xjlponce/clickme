67b81d8187898349a17bf794bed3545538989778
xjlponce <98378615+xjlponce@users.noreply.github.com>
2023-04-20 11:12:16 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,16 +12,12 @@ function App() {
   const audioRef = useRef(null);
 
   useEffect(() => {
-    if (time > 0 && !showImage) {
-      const timer = setTimeout(() => setTime(time - 1), 1000);
-      return () => clearTimeout(timer);
-    }
-  }, [time, showImage]);
-
-  useEffect(() => {
-    if (time === 0 && !showImage) {
+    if (time === 0 && !showImage && audioRef.current) {
       setShowImage(true);
       audioRef.current.play(); // autoplay the audio
+    } else if (time > 0 && !showImage) {
+      const timer = setTimeout(() => setTime(time - 1), 1000);
+      return () => clearTimeout(timer);
     }
   }, [time, showImage]);
 
@@ -33,12 +29,15 @@ function App() {
         setShowImage(true);
       }
     }
-
+  
+    const maxWidth = window.innerWidth * 0.9;
+    const maxHeight = window.innerHeight * 0.9;
     setButtonPosition({
-      x: Math.random() * window.innerWidth,
-      y: Math.random() * window.innerHeight,
+      x: Math.random() * maxWidth,
+      y: Math.random() * maxHeight,
     });
   };
+  
 
   const handleMouseMove = (event) => {
     if (buttonRef.current) {
@@ -66,7 +65,7 @@ function App() {
     padding: '12px 24px',
   };
 
-  const isCursorInButton = () => {
+  const isCursorInButton = (event) => {
     if (buttonRef.current) {
       const buttonRect = buttonRef.current.getBoundingClientRect();
       const isCursorInButton = (
@@ -79,3 +78,19 @@ function App() {
     }
     return false;
   };
+  
+  return (
+    <div className="App" onMouseMove={handleMouseMove}>
+      {showImage && <img src="https://cdn3.whatculture.com/images/2019/01/0a474a9ae0b07ce8-1200x675.jpg" alt=""/>}
+      <button ref={buttonRef} style={buttonStyle} onClick={handleClick}>
+        Click Me
+      </button>
+      <div className="score">Score: {score}</div>
+      <div className="timer">Time: {time}s</div>
+      <audio ref={audioRef} src="C:\Users\Admin\Desktop\sart\my-app\src\onlymp3.to - Scary Scream Sound Effect HD-F2hvl2iOI8k-256k-1656015137467.mp3" autoPlay />
+
+    </div>
+  );
+}
+
+export default App;
